4653d83c64013d0151be8ea5a260ed4f93e6b03a
Bjorn Reese <breese@users.sourceforge.net>
2020-05-08 13:34:59 +0200

    Added constexpr_copy algorithm

--- test/heap/algorithm_constexpr_suite.cpp
+++ test/heap/algorithm_constexpr_suite.cpp
@@ -9,6 +9,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <vista/constexpr/array.hpp>
+#include <vista/constexpr/algorithm.hpp>
 #include <vista/heap/algorithm.hpp>
 
 using namespace vista;
@@ -16,23 +17,53 @@ using namespace vista;
 //-----------------------------------------------------------------------------
 
 template <typename T, typename Compare = constexpr_less<T>>
-struct heap_factory
+class heap_factory
 {
-    template <typename Array, typename Arg>
-    static constexpr void emplace(Array& array, std::size_t index, Arg&& arg)
-    {
-        array[index] = arg;
-        heap::push<true>(array.begin(), array.begin() + index + 1, Compare{});
-    }
-
+public:
     template <typename... Args>
     static constexpr auto make(Args&&... args) -> vista::constexpr_array<T, sizeof...(args)>
     {
         vista::constexpr_array<T, sizeof...(args)> result{};
-        auto k = 0U;
-        int expander[] = { 0, ((void)emplace(result, k++, std::forward<Args>(args)), 0)... };
-        (void)expander;
+        insert(result.begin(), result.begin(), std::forward<Args>(args)...);
         return result;
+    }
+
+    template <std::size_t N>
+    static constexpr auto pop(vista::constexpr_array<T, N> original)
+    {
+        heap::pop<true>(original.begin(), original.end(), Compare{});
+        vista::constexpr_array<T, N - 1> result{};
+        constexpr_copy(original.begin(), original.end() - 1, result.begin());
+        return result;
+    }
+
+    template <std::size_t N, typename... Args>
+    static constexpr auto append(const vista::constexpr_array<T, N>& original,
+                                 Args&&... args) -> vista::constexpr_array<T, N + sizeof...(args)>
+    {
+        vista::constexpr_array<T, N + sizeof...(args)> result{};
+        constexpr_copy(original.begin(), original.end(), result.begin());
+        insert(result.begin(), result.begin() + N, std::forward<Args>(args)...);
+        return result;
+    }
+
+private:
+    template <typename RandomAccessIterator, typename Arg>
+    static constexpr void insert(RandomAccessIterator first,
+                                 RandomAccessIterator position,
+                                 Arg&& arg)
+    {
+        *position = arg;
+        heap::push<true>(first, position + 1, Compare{});
+    }
+
+    template <typename RandomAccessIterator, typename... Args>
+    static constexpr void insert(RandomAccessIterator first,
+                                 RandomAccessIterator position,
+                                 Args&&... args)
+    {
+        int expander[] = { 0, ((void)insert(first, position++, std::forward<Args>(args)), 0)... };
+        (void)expander;
     }
 };
 
@@ -40,6 +71,7 @@ struct heap_factory
 
 constexpr auto increasing = heap_factory<int>::make(11, 22, 33, 44);
 
+static_assert(increasing.size() == 4, "");
 static_assert(increasing[0] == 44, "");
 static_assert(increasing[1] == 33, "");
 static_assert(increasing[2] == 22, "");
@@ -47,6 +79,7 @@ static_assert(increasing[3] == 11, "");
 
 constexpr auto increasing_predicate = heap_factory<int, constexpr_greater<int>>::make(11, 22, 33, 44);
 
+static_assert(increasing_predicate.size() == 4, "");
 static_assert(increasing_predicate[0] == 11, "");
 static_assert(increasing_predicate[1] == 22, "");
 static_assert(increasing_predicate[2] == 33, "");
@@ -54,6 +87,7 @@ static_assert(increasing_predicate[3] == 44, "");
 
 constexpr auto decreasing = heap_factory<int>::make(44, 33, 22, 11);
 
+static_assert(decreasing.size() == 4, "");
 static_assert(decreasing[0] == 44, "");
 static_assert(decreasing[1] == 33, "");
 static_assert(decreasing[2] == 22, "");
@@ -61,7 +95,43 @@ static_assert(decreasing[3] == 11, "");
 
 constexpr auto decreasing_predicate = heap_factory<int, constexpr_greater<int>>::make(44, 33, 22, 11);
 
+static_assert(decreasing_predicate.size() == 4, "");
 static_assert(decreasing_predicate[0] == 11, "");
 static_assert(decreasing_predicate[1] == 22, "");
 static_assert(decreasing_predicate[2] == 33, "");
 static_assert(decreasing_predicate[3] == 44, "");
+
+//-----------------------------------------------------------------------------
+
+constexpr auto increasing_popped = heap_factory<int>::pop(increasing);
+
+static_assert(increasing_popped.size() == 3, "");
+static_assert(increasing_popped[0] == 33, "");
+static_assert(increasing_popped[1] == 11, "");
+static_assert(increasing_popped[2] == 22, "");
+
+constexpr auto increasing_predicate_popped = heap_factory<int, constexpr_greater<int>>::pop(increasing_predicate);
+
+static_assert(increasing_predicate_popped.size() == 3, "");
+static_assert(increasing_predicate_popped[0] == 22, "");
+static_assert(increasing_predicate_popped[1] == 44, "");
+static_assert(increasing_predicate_popped[2] == 33, "");
+
+constexpr auto decreasing_popped = heap_factory<int>::pop(decreasing);
+
+static_assert(decreasing_popped.size() == 3, "");
+static_assert(decreasing_popped[0] == 33, "");
+static_assert(decreasing_popped[1] == 11, "");
+static_assert(decreasing_popped[2] == 22, "");
+
+//-----------------------------------------------------------------------------
+
+constexpr auto increasing_appended = heap_factory<int>::append(increasing, 55, 66);
+
+static_assert(increasing_appended.size() == 6, "");
+static_assert(increasing_appended[0] == 66, "");
+static_assert(increasing_appended[1] == 44, "");
+static_assert(increasing_appended[2] == 55, "");
+static_assert(increasing_appended[3] == 11, "");
+static_assert(increasing_appended[4] == 33, "");
+static_assert(increasing_appended[5] == 22, "");
